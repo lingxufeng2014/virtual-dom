e266f6b1d01e362a83b3af9b75301a55d3744f6e
lingxufeng <lingxufeng@bytedance.com>
2018-08-21 00:05:54 +0800

    格式

--- vtree/diff-props.js
+++ vtree/diff-props.js
@@ -1,58 +1,64 @@
-var isObject = require("is-object")
-var isHook = require("../vnode/is-vhook")
+var isObject = require('is-object');
+var isHook = require('../vnode/is-vhook');
 
-module.exports = diffProps
+module.exports = diffProps;
 
 function diffProps(a, b) {
-    var diff
+    var diff;
 
-    for (var aKey in a) {
+    for(var aKey in a) {
         if (!(aKey in b)) {
-            diff = diff || {}
-            diff[aKey] = undefined
+            diff = diff || {};
+            diff[aKey] = undefined;
         }
 
-        var aValue = a[aKey]
-        var bValue = b[aKey]
+        var aValue = a[aKey];
+        var bValue = b[aKey];
 
         if (aValue === bValue) {
-            continue
-        } else if (isObject(aValue) && isObject(bValue)) {
+            continue;
+        }
+        else if (isObject(aValue) && isObject(bValue)) {
             if (getPrototype(bValue) !== getPrototype(aValue)) {
-                diff = diff || {}
-                diff[aKey] = bValue
-            } else if (isHook(bValue)) {
-                 diff = diff || {}
-                 diff[aKey] = bValue
-            } else {
-                var objectDiff = diffProps(aValue, bValue)
+                diff = diff || {};
+                diff[aKey] = bValue;
+            }
+            else if (isHook(bValue)) {
+                diff = diff || {};
+                diff[aKey] = bValue;
+            }
+            else {
+                var objectDiff = diffProps(aValue, bValue);
                 if (objectDiff) {
-                    diff = diff || {}
-                    diff[aKey] = objectDiff
+                    diff = diff || {};
+                    diff[aKey] = objectDiff;
                 }
             }
-        } else {
-            diff = diff || {}
-            diff[aKey] = bValue
+        }
+        else {
+            diff = diff || {};
+            diff[aKey] = bValue;
         }
     }
 
-    for (var bKey in b) {
+    for(var bKey in b) {
         if (!(bKey in a)) {
-            diff = diff || {}
-            diff[bKey] = b[bKey]
+            diff = diff || {};
+            diff[bKey] = b[bKey];
         }
     }
 
-    return diff
+    return diff;
 }
 
 function getPrototype(value) {
-  if (Object.getPrototypeOf) {
-    return Object.getPrototypeOf(value)
-  } else if (value.__proto__) {
-    return value.__proto__
-  } else if (value.constructor) {
-    return value.constructor.prototype
-  }
+    if (Object.getPrototypeOf) {
+        return Object.getPrototypeOf(value);
+    }
+    else if (value.__proto__) {
+        return value.__proto__;
+    }
+    else if (value.constructor) {
+        return value.constructor.prototype;
+    }
 }
--- vtree/diff.js
+++ vtree/diff.js
@@ -1,112 +1,120 @@
-var isArray = require("x-is-array")
+var isArray = require('x-is-array');
 
-var VPatch = require("../vnode/vpatch")
-var isVNode = require("../vnode/is-vnode")
-var isVText = require("../vnode/is-vtext")
-var isWidget = require("../vnode/is-widget")
-var isThunk = require("../vnode/is-thunk")
-var handleThunk = require("../vnode/handle-thunk")
+var VPatch = require('../vnode/vpatch');
+var isVNode = require('../vnode/is-vnode');
+var isVText = require('../vnode/is-vtext');
+var isWidget = require('../vnode/is-widget');
+var isThunk = require('../vnode/is-thunk');
+var handleThunk = require('../vnode/handle-thunk');
 
-var diffProps = require("./diff-props")
+var diffProps = require('./diff-props');
 
-module.exports = diff
+module.exports = diff;
 
 function diff(a, b) {
-    var patch = { a: a }
-    walk(a, b, patch, 0)
-    return patch
+    var patch = {a: a};
+    walk(a, b, patch, 0);
+    return patch;
 }
 
 function walk(a, b, patch, index) {
     if (a === b) {
-        return
+        return;
     }
 
-    var apply = patch[index]
-    var applyClear = false
+    var apply = patch[index];
+    var applyClear = false;
 
     if (isThunk(a) || isThunk(b)) {
-        thunks(a, b, patch, index)
-    } else if (b == null) {
+        thunks(a, b, patch, index);
+    }
+    else if (b == null) {
 
         // If a is a widget we will add a remove patch for it
         // Otherwise any child widgets/hooks must be destroyed.
         // This prevents adding two remove patches for a widget.
         if (!isWidget(a)) {
-            clearState(a, patch, index)
-            apply = patch[index]
+            clearState(a, patch, index);
+            apply = patch[index];
         }
 
-        apply = appendPatch(apply, new VPatch(VPatch.REMOVE, a, b))
-    } else if (isVNode(b)) {
+        apply = appendPatch(apply, new VPatch(VPatch.REMOVE, a, b));
+    }
+    else if (isVNode(b)) {
         if (isVNode(a)) {
             if (a.tagName === b.tagName &&
                 a.namespace === b.namespace &&
                 a.key === b.key) {
-                var propsPatch = diffProps(a.properties, b.properties)
+                var propsPatch = diffProps(a.properties, b.properties);
                 if (propsPatch) {
                     apply = appendPatch(apply,
-                        new VPatch(VPatch.PROPS, a, propsPatch))
+                        new VPatch(VPatch.PROPS, a, propsPatch));
                 }
-                apply = diffChildren(a, b, patch, apply, index)
-            } else {
-                apply = appendPatch(apply, new VPatch(VPatch.VNODE, a, b))
-                applyClear = true
+                apply = diffChildren(a, b, patch, apply, index);
             }
-        } else {
-            apply = appendPatch(apply, new VPatch(VPatch.VNODE, a, b))
-            applyClear = true
+            else {
+                apply = appendPatch(apply, new VPatch(VPatch.VNODE, a, b));
+                applyClear = true;
+            }
         }
-    } else if (isVText(b)) {
+        else {
+            apply = appendPatch(apply, new VPatch(VPatch.VNODE, a, b));
+            applyClear = true;
+        }
+    }
+    else if (isVText(b)) {
         if (!isVText(a)) {
-            apply = appendPatch(apply, new VPatch(VPatch.VTEXT, a, b))
-            applyClear = true
-        } else if (a.text !== b.text) {
-            apply = appendPatch(apply, new VPatch(VPatch.VTEXT, a, b))
+            apply = appendPatch(apply, new VPatch(VPatch.VTEXT, a, b));
+            applyClear = true;
         }
-    } else if (isWidget(b)) {
+        else if (a.text !== b.text) {
+            apply = appendPatch(apply, new VPatch(VPatch.VTEXT, a, b));
+        }
+    }
+    else if (isWidget(b)) {
         if (!isWidget(a)) {
-            applyClear = true
+            applyClear = true;
         }
 
-        apply = appendPatch(apply, new VPatch(VPatch.WIDGET, a, b))
+        apply = appendPatch(apply, new VPatch(VPatch.WIDGET, a, b));
     }
 
     if (apply) {
-        patch[index] = apply
+        patch[index] = apply;
     }
 
     if (applyClear) {
-        clearState(a, patch, index)
+        clearState(a, patch, index);
     }
 }
 
 function diffChildren(a, b, patch, apply, index) {
-    var aChildren = a.children
-    var orderedSet = reorder(aChildren, b.children)
-    var bChildren = orderedSet.children
+    var aChildren = a.children;
+    var orderedSet = reorder(aChildren, b.children);
+    var bChildren = orderedSet.children;
 
-    var aLen = aChildren.length
-    var bLen = bChildren.length
-    var len = aLen > bLen ? aLen : bLen
+    var aLen = aChildren.length;
+    var bLen = bChildren.length;
+    var len = aLen > bLen ? aLen : bLen;
 
-    for (var i = 0; i < len; i++) {
-        var leftNode = aChildren[i]
-        var rightNode = bChildren[i]
-        index += 1
+    for(var i = 0; i < len; i++) {
+        var leftNode = aChildren[i];
+        var rightNode = bChildren[i];
+        index += 1;
 
         if (!leftNode) {
             if (rightNode) {
                 // Excess nodes in b need to be added
                 apply = appendPatch(apply,
-                    new VPatch(VPatch.INSERT, null, rightNode))
+                    new VPatch(VPatch.INSERT, null, rightNode));
             }
-        } else {
-            walk(leftNode, rightNode, patch, index)
+        }
+        else {
+            walk(leftNode, rightNode, patch, index);
         }
 
         if (isVNode(leftNode) && leftNode.count) {
-            index += leftNode.count
+            index += leftNode.count;
         }
     }
 
@@ -116,63 +124,65 @@ function diffChildren(a, b, patch, apply, index) {
             VPatch.ORDER,
             a,
             orderedSet.moves
-        ))
+        ));
     }
 
-    return apply
+    return apply;
 }
 
 function clearState(vNode, patch, index) {
     // TODO: Make this a single walk, not two
-    unhook(vNode, patch, index)
-    destroyWidgets(vNode, patch, index)
+    unhook(vNode, patch, index);
+    destroyWidgets(vNode, patch, index);
 }
 
 // Patch records for all destroyed widgets must be added because we need
 // a DOM node reference for the destroy function
 function destroyWidgets(vNode, patch, index) {
     if (isWidget(vNode)) {
-        if (typeof vNode.destroy === "function") {
+        if (typeof vNode.destroy === 'function') {
             patch[index] = appendPatch(
                 patch[index],
                 new VPatch(VPatch.REMOVE, vNode, null)
-            )
+            );
         }
-    } else if (isVNode(vNode) && (vNode.hasWidgets || vNode.hasThunks)) {
-        var children = vNode.children
-        var len = children.length
-        for (var i = 0; i < len; i++) {
-            var child = children[i]
-            index += 1
+    }
+    else if (isVNode(vNode) && (vNode.hasWidgets || vNode.hasThunks)) {
+        var children = vNode.children;
+        var len = children.length;
+        for(var i = 0; i < len; i++) {
+            var child = children[i];
+            index += 1;
 
-            destroyWidgets(child, patch, index)
+            destroyWidgets(child, patch, index);
 
             if (isVNode(child) && child.count) {
-                index += child.count
+                index += child.count;
             }
         }
-    } else if (isThunk(vNode)) {
-        thunks(vNode, null, patch, index)
+    }
+    else if (isThunk(vNode)) {
+        thunks(vNode, null, patch, index);
     }
 }
 
 // Create a sub-patch for thunks
 function thunks(a, b, patch, index) {
-    var nodes = handleThunk(a, b)
-    var thunkPatch = diff(nodes.a, nodes.b)
+    var nodes = handleThunk(a, b);
+    var thunkPatch = diff(nodes.a, nodes.b);
     if (hasPatches(thunkPatch)) {
-        patch[index] = new VPatch(VPatch.THUNK, null, thunkPatch)
+        patch[index] = new VPatch(VPatch.THUNK, null, thunkPatch);
     }
 }
 
 function hasPatches(patch) {
-    for (var index in patch) {
-        if (index !== "a") {
-            return true
+    for(var index in patch) {
+        if (index !== 'a') {
+            return true;
         }
     }
 
-    return false
+    return false;
 }
 
 // Execute hooks when two nodes are identical
@@ -186,139 +196,144 @@ function unhook(vNode, patch, index) {
                     vNode,
                     undefinedKeys(vNode.hooks)
                 )
-            )
+            );
         }
 
         if (vNode.descendantHooks || vNode.hasThunks) {
-            var children = vNode.children
-            var len = children.length
-            for (var i = 0; i < len; i++) {
-                var child = children[i]
-                index += 1
+            var children = vNode.children;
+            var len = children.length;
+            for(var i = 0; i < len; i++) {
+                var child = children[i];
+                index += 1;
 
-                unhook(child, patch, index)
+                unhook(child, patch, index);
 
                 if (isVNode(child) && child.count) {
-                    index += child.count
+                    index += child.count;
                 }
             }
         }
-    } else if (isThunk(vNode)) {
-        thunks(vNode, null, patch, index)
+    }
+    else if (isThunk(vNode)) {
+        thunks(vNode, null, patch, index);
     }
 }
 
 function undefinedKeys(obj) {
-    var result = {}
+    var result = {};
 
-    for (var key in obj) {
-        result[key] = undefined
+    for(var key in obj) {
+        result[key] = undefined;
     }
 
-    return result
+    return result;
 }
 
 // List diff, naive left to right reordering
 function reorder(aChildren, bChildren) {
     // O(M) time, O(M) memory
-    var bChildIndex = keyIndex(bChildren)
-    var bKeys = bChildIndex.keys
-    var bFree = bChildIndex.free
+    var bChildIndex = keyIndex(bChildren);
+    var bKeys = bChildIndex.keys;
+    var bFree = bChildIndex.free;
 
     if (bFree.length === bChildren.length) {
         return {
             children: bChildren,
             moves: null
-        }
+        };
     }
 
     // O(N) time, O(N) memory
-    var aChildIndex = keyIndex(aChildren)
-    var aKeys = aChildIndex.keys
-    var aFree = aChildIndex.free
+    var aChildIndex = keyIndex(aChildren);
+    var aKeys = aChildIndex.keys;
+    var aFree = aChildIndex.free;
 
     if (aFree.length === aChildren.length) {
         return {
             children: bChildren,
             moves: null
-        }
+        };
     }
 
     // O(MAX(N, M)) memory
-    var newChildren = []
+    var newChildren = [];
 
-    var freeIndex = 0
-    var freeCount = bFree.length
-    var deletedItems = 0
+    var freeIndex = 0;
+    var freeCount = bFree.length;
+    var deletedItems = 0;
 
     // Iterate through a and match a node in b
     // O(N) time,
-    for (var i = 0 ; i < aChildren.length; i++) {
-        var aItem = aChildren[i]
-        var itemIndex
+    for(var i = 0; i < aChildren.length; i++) {
+        var aItem = aChildren[i];
+        var itemIndex;
 
         if (aItem.key) {
             if (bKeys.hasOwnProperty(aItem.key)) {
                 // Match up the old keys
-                itemIndex = bKeys[aItem.key]
-                newChildren.push(bChildren[itemIndex])
+                itemIndex = bKeys[aItem.key];
+                newChildren.push(bChildren[itemIndex]);
 
-            } else {
-                // Remove old keyed items
-                itemIndex = i - deletedItems++
-                newChildren.push(null)
             }
-        } else {
+            else {
+                // Remove old keyed items
+                itemIndex = i - deletedItems++;
+                newChildren.push(null);
+            }
+        }
+        else {
             // Match the item in a with the next free item in b
             if (freeIndex < freeCount) {
-                itemIndex = bFree[freeIndex++]
-                newChildren.push(bChildren[itemIndex])
-            } else {
+                itemIndex = bFree[freeIndex++];
+                newChildren.push(bChildren[itemIndex]);
+            }
+            else {
                 // There are no free items in b to match with
                 // the free items in a, so the extra free nodes
                 // are deleted.
-                itemIndex = i - deletedItems++
-                newChildren.push(null)
+                itemIndex = i - deletedItems++;
+                newChildren.push(null);
             }
         }
     }
 
     var lastFreeIndex = freeIndex >= bFree.length ?
         bChildren.length :
-        bFree[freeIndex]
+        bFree[freeIndex];
 
     // Iterate through b and append any new keys
     // O(M) time
-    for (var j = 0; j < bChildren.length; j++) {
-        var newItem = bChildren[j]
+    for(var j = 0; j < bChildren.length; j++) {
+        var newItem = bChildren[j];
 
         if (newItem.key) {
             if (!aKeys.hasOwnProperty(newItem.key)) {
                 // Add any new keyed items
                 // We are adding new items to the end and then sorting them
                 // in place. In future we should insert new items in place.
-                newChildren.push(newItem)
+                newChildren.push(newItem);
             }
-        } else if (j >= lastFreeIndex) {
+        }
+        else if (j >= lastFreeIndex) {
             // Add any leftover non-keyed items
-            newChildren.push(newItem)
+            newChildren.push(newItem);
         }
     }
 
-    var simulate = newChildren.slice()
-    var simulateIndex = 0
-    var removes = []
-    var inserts = []
-    var simulateItem
+    var simulate = newChildren.slice();
+    var simulateIndex = 0;
+    var removes = [];
+    var inserts = [];
+    var simulateItem;
 
-    for (var k = 0; k < bChildren.length;) {
-        var wantedItem = bChildren[k]
-        simulateItem = simulate[simulateIndex]
+    for(var k = 0; k < bChildren.length;) {
+        var wantedItem = bChildren[k];
+        simulateItem = simulate[simulateIndex];
 
         // remove items
-        while (simulateItem === null && simulate.length) {
-            removes.push(remove(simulate, simulateIndex, null))
-            simulateItem = simulate[simulateIndex]
+        while(simulateItem === null && simulate.length) {
+            removes.push(remove(simulate, simulateIndex, null));
+            simulateItem = simulate[simulateIndex];
         }
 
         if (!simulateItem || simulateItem.key !== wantedItem.key) {
@@ -327,41 +342,41 @@ function reorder(aChildren, bChildren) {
                 if (simulateItem && simulateItem.key) {
                     // if an insert doesn't put this key in place, it needs to move
                     if (bKeys[simulateItem.key] !== k + 1) {
-                        removes.push(remove(simulate, simulateIndex, simulateItem.key))
-                        simulateItem = simulate[simulateIndex]
+                        removes.push(remove(simulate, simulateIndex, simulateItem.key));
+                        simulateItem = simulate[simulateIndex];
                         // if the remove didn't put the wanted item in place, we need to insert it
                         if (!simulateItem || simulateItem.key !== wantedItem.key) {
-                            inserts.push({key: wantedItem.key, to: k})
+                            inserts.push({key: wantedItem.key, to: k});
                         }
                         // items are matching, so skip ahead
                         else {
-                            simulateIndex++
+                            simulateIndex++;
                         }
                     }
                     else {
-                        inserts.push({key: wantedItem.key, to: k})
+                        inserts.push({key: wantedItem.key, to: k});
                     }
                 }
                 else {
-                    inserts.push({key: wantedItem.key, to: k})
+                    inserts.push({key: wantedItem.key, to: k});
                 }
-                k++
+                k++;
             }
             // a key in simulate has no matching wanted key, remove it
             else if (simulateItem && simulateItem.key) {
-                removes.push(remove(simulate, simulateIndex, simulateItem.key))
+                removes.push(remove(simulate, simulateIndex, simulateItem.key));
             }
         }
         else {
-            simulateIndex++
-            k++
+            simulateIndex++;
+            k++;
         }
     }
 
     // remove all the remaining nodes from simulate
     while(simulateIndex < simulate.length) {
-        simulateItem = simulate[simulateIndex]
-        removes.push(remove(simulate, simulateIndex, simulateItem && simulateItem.key))
+        simulateItem = simulate[simulateIndex];
+        removes.push(remove(simulate, simulateIndex, simulateItem && simulateItem.key));
     }
 
     // If the only moves we have are deletes then we can just
@@ -370,7 +385,7 @@ function reorder(aChildren, bChildren) {
         return {
             children: newChildren,
             moves: null
-        }
+        };
     }
 
     return {
@@ -379,49 +394,52 @@ function reorder(aChildren, bChildren) {
             removes: removes,
             inserts: inserts
         }
-    }
+    };
 }
 
 function remove(arr, index, key) {
-    arr.splice(index, 1)
+    arr.splice(index, 1);
 
     return {
         from: index,
         key: key
-    }
+    };
 }
 
 function keyIndex(children) {
-    var keys = {}
-    var free = []
-    var length = children.length
+    var keys = {};
+    var free = [];
+    var length = children.length;
 
-    for (var i = 0; i < length; i++) {
-        var child = children[i]
+    for(var i = 0; i < length; i++) {
+        var child = children[i];
 
         if (child.key) {
-            keys[child.key] = i
-        } else {
-            free.push(i)
+            keys[child.key] = i;
+        }
+        else {
+            free.push(i);
         }
     }
 
     return {
         keys: keys,     // A hash of key name to index
         free: free      // An array of unkeyed item indices
-    }
+    };
 }
 
 function appendPatch(apply, patch) {
     if (apply) {
         if (isArray(apply)) {
-            apply.push(patch)
-        } else {
-            apply = [apply, patch]
+            apply.push(patch);
+        }
+        else {
+            apply = [apply, patch];
         }
 
-        return apply
-    } else {
-        return patch
+        return apply;
+    }
+    else {
+        return patch;
     }
 }
